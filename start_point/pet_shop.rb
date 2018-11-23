7d3051efd9721e0ea3ec49a94ac0c9265c3765d0
Michael Conner <michaelconner@hotmail.co.uk>
2018-11-23 17:11:25 +0000

    tests 2-6 completed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,5 +1,21 @@
 
 
-def pet_shop_name(name)
-  return name[:name]
+def pet_shop_name(shop)
+  return shop[:name]
+end
+
+def total_cash(shop)
+  return shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(shop, amount)
+  return shop[:admin][:total_cash] += amount
+end
+
+def pets_sold(shop)
+  return shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(shop, amount_sold)
+  return shop[:admin][:pets_sold] += amount_sold
 end
